27d9067033c5b7b84d374ce8597566bc615ba1d4
mromeo1992 <mattia.romeo@community.unipa.it>
2023-11-29 17:43:59 +0100

    fix error

--- set_environ.sh
+++ set_environ.sh
@@ -4,6 +4,7 @@ cd $CONDA_PREFIX
 mkdir -p ./etc/conda/activate.d && touch ./etc/conda/activate.d/env_vars.sh
 mkdir -p ./etc/conda/deactivate.d && touch ./etc/conda/deactivate.d/env_vars.sh
 printf "\n%s\n" "export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH" >> ./etc/conda/activate.d/env_vars.sh
+printf "\n%s\n" "unset LD_LIBRARY_PATH" >> ./etc/conda/deactivate.d/env_vars.sh
 cd $DELTA_BIT
 
 
