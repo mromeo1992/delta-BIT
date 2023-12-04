5d1740f7c0133b3d8d2e7160ec14ac753aa400d8
mromeo1992 <mattia.romeo@community.unipa.it>
2023-12-04 11:38:32 +0100

    full test pipeline update

--- test_pipeline/testing/full_prediction.py
+++ test_pipeline/testing/full_prediction.py
@@ -30,17 +30,14 @@ def main():
     parser.add_argument("-m","--models",choices=models, help="Insert here the name of the models you want to use",default='pretrained')
     parser.add_argument("--data_type", help="Insert here the extention of the disired output data: possibility nii, nii.gz, mgz",default='nii.gz')
     parser.add_argument("-dir", "--dataset_directory", help="indicate here your main folder which cointains your dataset", required=True)
-    parser.add_argument("--T1_path", help="indicate here the T1 image's relative pathname (starting from the subject's folder)", default='T1.nii.gz')    
-    parser.add_argument("--dwi_path", help="indicate here the DWI image's relative pathname (starting from the subject's folder)", default='DWI.nii.gz')
+    parser.add_argument("--T1_path", help="file name of the T1 image", default='T1.nii.gz')    
+    parser.add_argument("--dwi_path", help="file name of the DWI image", default='DWI.nii.gz')
     parser.add_argument("--bvecs", help="file name of the bvecs file", default='DWI.bvec')
     parser.add_argument("--bvals", help="file name of the bvals file", default='DWI.bval')
-    parser.add_argument("--registration",action="store_true", help="Inser if you data have already been registered on a standard template")
-    parser.add_argument("-o", "--output_dir", help="output_directory",default=home+"/project_name")    
-    
-    
-    parser.add_argument("-n", "--name", help="Project's name", required=True)
+    parser.add_argument("--registration",action="store_true", help="Inser if you data have already been registered on a standard template")    
     parser.add_argument("--tmp", action='store_true', help="Insert this flag if you want to keep temporary files")
     parser.add_argument("--cortex_area",nargs='+', help="Insert here the cortex area you want to predict\n choose between {}".format(models+['all']), default=['all'])
+    parser.add_argument("-o", "--output_dir", help="output_directory",default=home+"/project_name")
     
 
     args = parser.parse_args()
@@ -49,7 +46,19 @@ def main():
     config = vars(args)
 
     name=config['name']
+    model=config['models']
+    datatype=config['data_type']
+    directory=config['dataset_directory']ù
+    T1=config['T1_path']
+    dwi=config['dwi_path']
+    bvecs=config['bvecs']
+    bvals=config['bvals']
+    reg=config['registration']
+    if reg==None:
+        reg=False
+    outfold=config['output_dir']
     cortex_area=config['cortex_area']
+
 
     print('\nYou select the subsequent areas: \n')
     print(cortex_area)
@@ -70,6 +79,15 @@ def main():
     tmp=config['tmp']
     if tmp==None:
         tmp=False
+
+    #initialise project
+    cmd='d-BIT_initialise -n '+name+' -m '+model+' --data_type '+datatype+' -dir '+directory+' --T1_path '+T1+' --dwi_path '+dwi+'  --bvecs '+bvecs+' --bvals '+bvals
+    if reg:
+        cmd=cmd+' --registration'
+    
+    cmd=cmd+' -o '+outfold
+    print('\nRunning: '+cmd)
+    os.system(cmd)
 
     #Preprocessing DWI
     cmd='d-BIT_preprocessDWI -n '+name
