85db19a7afbbfa301f08360aff510a7432db9945
mromeo1992 <mattia.romeo@community.unipa.it>
2023-12-05 12:01:22 +0100

    add uninstall file

---
+++ uninstall.sh
@@ -0,0 +1,54 @@
+#!bin/bash
+
+eval "$(conda shell.bash hook)"
+
+
+check=true
+while $check
+do
+    echo 'Do you want to uninstall delta-BIT? [y]es [n]o'
+
+    read varname
+
+    if [ "$varname" = y ]
+    then
+        check=false
+        echo Removing delta-BIT
+        conda deactivate
+        conda activate base
+
+        python setup.py --record files.txt
+        xargs rm -rf < files.txt
+        check2=true
+        
+        while $check2
+        do
+            echo "Do you want to remove conda environment? [y]es [n]o"
+
+            read varname
+            
+            if [ "$varname" = y ]
+            then
+                check2=false
+                echo "Removing conda environment: conda remove --name delta-BIT --all"
+                conda remove --name delta-BIT --all
+                conda clean -a
+            
+            elif [ "$varname" = n ]
+            then
+                check2=false
+                break
+      
+            fi
+    
+    elif [ "$varname" = n ]
+    then
+        check=false
+        echo Exting
+        break
+
+
+
+
+    fi
+done
