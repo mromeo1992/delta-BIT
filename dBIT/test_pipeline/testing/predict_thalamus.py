e4e9f6aa95b4c2438a5603616259c6e9dbbd525b
mromeo1992 <mattia.romeo@community.unipa.it>
2023-12-11 16:16:41 +0100

    add license

--- dBIT/test_pipeline/testing/predict_thalamus.py
+++ dBIT/test_pipeline/testing/predict_thalamus.py
@@ -1,3 +1,21 @@
+#delta-BIT is a progragram for tractography prediction
+#Copyright (C) 2023,  University of Palermo, department of Physics 
+#and Chemistry, Palermo, Italy and National Institue of Nuclear Physics (INFN), Italy
+#
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import keras
 import sys
 import os
