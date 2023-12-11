25d44ce6bf38d60ce4709eeef2509f8825a94c77
mromeo1992 <mattia.romeo@community.unipa.it>
2023-12-11 15:48:55 +0100

    fix error

--- dBIT/__init__.py
+++ dBIT/__init__.py
@@ -1,9 +1,12 @@
 start = "\033[1m"
 end = "\033[0;0m"
-line=start+" delta-BIT IS NOT INTENDED FOR CLINICAL USE! "+end
-lenght=len(line)
+
+line1="By using delta-BIT, you acknowledge that:\n\n"
+
+line2=start+"IT IS NOT INTENDED FOR CLINICAL USE BUT INSTEAD PURELY ACADEMIC (RESEARCH) SOFTWARE"+end
+lenght=len(line2)
 n_space=12
-newline=line.center(n_space*2+lenght,'*')
+newline=line1+line2.center(n_space*2+lenght)
 print('\n')
 print(newline)
 print('\n\n')
